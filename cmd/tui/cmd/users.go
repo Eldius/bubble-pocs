84acc6e4f58d1dbf19764a5963fc9c827ae887dd
eldius <eldiosantos@gmail.com>
2024-10-22 22:53:18 -0300

    Some more code...

--- cmd/tui/cmd/users.go
+++ cmd/tui/cmd/users.go
@@ -11,13 +11,8 @@ import (
 // usersCmd represents the users command
 var usersCmd = &cobra.Command{
 	Use:   "users",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Lists users' info from Mojang API",
+	Long:  `Lists users' info from Mojang API.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		ui.Start(args...)
 	},
